55a684e3c91ede99ee803fe55602a0d427b8704b
asal <asal13adib78@gmail.com>
2022-07-03 15:58:07 +0430

    make alerts style and their actions

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,12 +11,29 @@ export class LoginComponent implements OnInit {
 
   constructor(
     private service: LoginService
-  ) { }
+     ) { }
 
   userInfo : LoginModel = new LoginModel();
   repetPassword!:any;
+  success!: boolean;
+  fail!: boolean;
 
   ngOnInit(): void {
+  }
+
+  postUserInfo(){
+    if(this.userInfo.password == this.repetPassword){
+      this.success = true;
+      this.fail = false;
+      this.service.PostDatas(this.userInfo).subscribe(
+        (data:any)=>{
+        }
+      )
+    }
+    else{
+      this.fail = true;
+      this.success = false
+    }
   }
 
 
